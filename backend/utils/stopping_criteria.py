5533c1d05687d1f085765e74e637d1cdcc503d53
bajajku <bajajku@sheridancollege.ca>
2025-06-16 15:52:22 -0400

    Refactor StopOnTokens class in stopping_criteria.py
    - Removed unnecessary newline and cleaned up code formatting in the StopOnTokens class.
    - Ensured consistent structure for the stopping criteria implementation.

--- backend/utils/stopping_criteria.py
+++ backend/utils/stopping_criteria.py
@@ -7,10 +7,9 @@ class StopOnTokens(StoppingCriteria):
         self.stop_list = ['\nHuman:', '\n```\n']
         self.stop_token_ids = [tokenizer(x)['input_ids'] for x in self.stop_list]
 
+
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
         for stop_ids in self.stop_token_ids:
             if torch.eq(input_ids[0][-len(stop_ids):], stop_ids).all():
                 return True
         return False
-
-stopping_criteria = StoppingCriteriaList([StopOnTokens()])
